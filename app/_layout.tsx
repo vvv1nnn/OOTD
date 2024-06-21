32ea49626ffcf36e07092c4c0912c98b73c9d0f8
Adriana Bamberger <adriana@bamberger.co.nz>
2024-06-21 13:48:06 +1200

    fixed outer layout page to bring back the login page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,48 +1,11 @@
-// THIS PAGE ISN'T DOING ANYTHING CURRENTLY
+import { Stack } from 'expo-router'
 
-// import TabBar from '@/components/TabBar'
-// import { Tabs } from 'expo-router'
-// import React from 'react'
-
-// export default function RootLayout() {
-//   return (
-//     <Tabs tabBar={(props) => <TabBar {...props} />}>
-//       <Tabs.Screen
-//         name="feed"
-//         options={{
-//           title: 'Feed',
-//         }}
-//       />
-//       <Tabs.Screen
-//         name="wardrobe"
-//         options={{
-//           title: 'Wardrobe',
-//         }}
-//       />
-//       <Tabs.Screen
-//         name="index"
-//         options={{
-//           title: 'Index',
-//         }}
-//       />
-//       {/* Down here we will need to add one here for the profile once we ahve the profile branch. */}
-//     </Tabs>
-//     // <Stack>
-//     //   <Stack.Screen name="index" />
-//     //   <Stack.Screen name="editprofile" />
-//     //   <Stack.Screen name="wardrobe" />
-//     // </Stack>
-//   )
-// }
-
-// import { Stack } from 'expo-router'
-
-// const StackLayout = () => {
-//   return (
-//     <Stack>
-//       <Stack.Screen name="index" />
-//       <Stack.Screen name="editprofile" />
-//       <Stack.Screen name="wardrobe" />
-//     </Stack>
-//   )
-// }
+const StackLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="editprofile" />
+      <Stack.Screen name="wardrobe" />
+    </Stack>
+  )
+}
